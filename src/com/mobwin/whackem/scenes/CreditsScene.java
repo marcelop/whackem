246b54fcd0737eaca2ef3acb4d92ac502878c802
Andre Macedo <andre.macedo@gmail.com>
2013-06-09 14:51:49 -0400

    Credits updated

--- src/com/mobwin/whackem/scenes/CreditsScene.java
+++ src/com/mobwin/whackem/scenes/CreditsScene.java
@@ -2,20 +2,14 @@ package com.mobwin.whackem.scenes;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import org.andengine.engine.Engine;
 import org.andengine.engine.handler.timer.ITimerCallback;
 import org.andengine.engine.handler.timer.TimerHandler;
 import org.andengine.entity.Entity;
-import org.andengine.entity.modifier.DelayModifier;
-import org.andengine.entity.modifier.FadeInModifier;
-import org.andengine.entity.modifier.FadeOutModifier;
 import org.andengine.entity.modifier.MoveModifier;
-import org.andengine.entity.modifier.ParallelEntityModifier;
-import org.andengine.entity.modifier.ScaleAtModifier;
-import org.andengine.entity.modifier.SequenceEntityModifier;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.AutoParallaxBackground;
 import org.andengine.entity.scene.background.ParallaxBackground;
@@ -23,14 +17,11 @@ import org.andengine.entity.scene.background.ParallaxBackground.ParallaxEntity;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.text.Text;
 import org.andengine.opengl.font.Font;
-import org.andengine.opengl.texture.TextureOptions;
-import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
-import org.andengine.opengl.texture.region.ITextureRegion;
 
 import tv.ouya.console.api.OuyaController;
-
 import android.annotation.SuppressLint;
+import android.util.Log;
 import android.view.KeyEvent;
 
 import com.mobwin.whackem.MainActivity;
@@ -48,22 +39,21 @@ public class CreditsScene extends Scene {
 	int numberOfTitles;	
 	private Sprite backgroundSprite;
 	public static final String MENU_BACK_STRING = "PRESS         TO RETURN";
+	final float duration = 10;
 	
+	final float initialY; //offscreen from the bottom
+	ArrayList<Entity> allLines;
+		
 	public CreditsScene(final Engine engine) {
 		
 		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
 		// Retrieve our font from the resource manager
 		this.engine = engine;
-		
-		numberOfTitles = 1;
-		
-		titles = new String[numberOfTitles];
-		names = new String[numberOfTitles][];
-		
+				
 		// Add return option at the bottom right corner of the screen
 		Font font = ResourceManager.getInstance().mFont;
 		
-		float x = MainActivity.WIDTH - 150;
+		float x = MainActivity.WIDTH - 180;
 		float y = font.getLineHeight();
 		
 		final Text returnOption = new Text(x, y, font, MENU_BACK_STRING, engine.getVertexBufferObjectManager());
@@ -79,32 +69,34 @@ public class CreditsScene extends Scene {
 		}));	
 		this.attachChild(returnOption);
 		
+		initialY = - font.getLineHeight();
 		
 		// Place credits centered in the screen, starting from the bottom
 		x = MainActivity.WIDTH / 2;
-		y = -20; //start offscreen		
+		y = initialY;		
 		
+		//number of titles in the credits
+		numberOfTitles = 2;
+		
+		titles = new String[numberOfTitles];
+		names = new String[numberOfTitles][];
 		
 		// Initialize all the titles and names you want in the credits
 		titles[0] = "People";
 		names[0] = new String[]{"Marcelo Perez","Andre Macedo"};
 		
+		titles[1] = "More People";
+		names[1] = new String[]{"Bruce Wayne","Pelé","Gengis Khan","Jenny Lui","Cloud Strife","KOS-MOS","Zubaba Bite","Bob Loblaw"};
 		
 		// include them in the screen
-		ArrayList<Entity> allLines = new ArrayList<Entity>();
+		allLines = new ArrayList<Entity>();
 		
 		for (int i = 0; i < numberOfTitles; i++) {
 			y = addToList(titles[i],names[i], allLines, x, y);
 		}
 		
-		// add all lines to the screen, and apply movement modifier
-		final float duration = 3;
-		
-		for (Entity t : allLines) {
-			t.registerEntityModifier(new MoveModifier(duration, x, t.getY(), x, t.getY() - y + MainActivity.HEIGHT));
-			this.attachChild(t);
-		}
-		
+		Log.d("All lines - font height", "" + font.getLineHeight());
+		for (Entity e : allLines)	Log.d("All Lines", "" + e.getY());
 		
 		// include background
 		backgroundSprite = new Sprite(MainActivity.WIDTH / 2 , MainActivity.HEIGHT/2,MainActivity.WIDTH, MainActivity.HEIGHT, ResourceManager.getInstance().mMenuBackgroundTextureRegion, engine.getVertexBufferObjectManager());
@@ -116,15 +108,32 @@ public class CreditsScene extends Scene {
 		setBackgroundEnabled(true);
 	}
 	
+	public void start() {
+		float x = MainActivity.WIDTH /2;
+		
+		//calculate the difference between the current and the original Y position
+	    float diffY = allLines.get(0).getY() - initialY;
+	    //calculate the distance covered
+	    float range = allLines.get(0).getY() - allLines.get(allLines.size() - 1).getY() + MainActivity.HEIGHT + ResourceManager.getInstance().mFont.getLineHeight()*2;
+	    		
+		for (Entity t : allLines) {
+			//unregister any modifier
+			t.clearEntityModifiers();
+			//apply movement modifier
+			t.registerEntityModifier(new MoveModifier(duration,x,t.getY() - diffY,x,t.getY() - diffY + range));
+		}
+	}	
 	
-	//adds the title followed by the list of names to a text list and return the y position of the last added entry, after spacing
+	// Adds the title followed by the list of names to a text list and the scene, and return the y position of the last added entry, after spacing
 	@SuppressLint("DefaultLocale")
 	private float addToList(String title, String[] names, List<Entity> list, float x, float y) {
 		Font font = ResourceManager.getInstance().mFont;
 			
+		y -= font.getLineHeight() + lineSpacing;
 		Text txtTitle = new Text(x,y,font,title.toUpperCase(),engine.getVertexBufferObjectManager());
 		list.add(txtTitle);
-		y -= font.getLineHeight() + lineSpacing*2;
+		this.attachChild(txtTitle);
+		y -= font.getLineHeight() + lineSpacing * 2;
 		
 		Arrays.sort(names);
 		
@@ -133,6 +142,7 @@ public class CreditsScene extends Scene {
 			txtName = new Text(x,y,font,name,engine.getVertexBufferObjectManager());
 			list.add(txtName);
 			y -= font.getLineHeight() + lineSpacing;
+			this.attachChild(txtName);
 		}
 		
 		return y;
